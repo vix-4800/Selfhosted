e151f6f0814ef0abb642a2ad74d75ba8571c19fc
Anton Vix <Gordan.ei@protonmail.ch>
2026-05-01 02:16:06 +0400

    refactor set_domain.sh and start.sh for improved readability and consistency

--- set_domain.sh
+++ set_domain.sh
@@ -2,39 +2,39 @@
 
 SERVICES_FILE="./data/homepage/services.yaml"
 
-if [ ! -f "$SERVICES_FILE" ]; then
-    echo "Error: file $SERVICES_FILE not found!"
-    exit 1
+if [[ ! -f "${SERVICES_FILE}" ]]; then
+  echo "Error: file ${SERVICES_FILE} not found!"
+  exit 1
 fi
 
 validate_domain() {
-    local domain=$1
-    local pattern="^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$"
-    if [[ "$domain" =~ $pattern ]]; then
-        return 0
-    else
-        return 1
-    fi
+  local domain=$1
+  local pattern="^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$"
+  if [[ "${domain}" =~ ${pattern} ]]; then
+    return 0
+  else
+    return 1
+  fi
 }
 
 # Get new domain
 new_domain=${1:-}
-if [ -z "$new_domain" ]; then
-    while true; do
-        read -p "Enter domain (without (http/https)://, example: example.com): " new_domain
-        if validate_domain "$new_domain"; then
-            break
-        else
-            echo "Domain is invalid!"
-        fi
-    done
+if [[ -z "${new_domain}" ]]; then
+  while true; do
+    read -p "Enter domain (without (http/https)://, example: example.com): " new_domain
+    if validate_domain "${new_domain}"; then
+      break
+    else
+      echo "Domain is invalid!"
+    fi
+  done
 fi
 
 # Backup
 backup_file="${SERVICES_FILE}.bak"
-cp "$SERVICES_FILE" "$backup_file"
-echo "Backup created: $backup_file"
+cp "${SERVICES_FILE}" "${backup_file}"
+echo "Backup created: ${backup_file}"
 
-sed -E "s|(href: http://)([a-zA-Z0-9-]+)\.[^/]+(/.*)|\1\2.${new_domain}\3|" "$SERVICES_FILE" > "${SERVICES_FILE}.tmp" && mv "${SERVICES_FILE}.tmp" "$SERVICES_FILE"
+sed -E "s|(href: http://)([a-zA-Z0-9-]+)\.[^/]+(/.*)|\1\2.${new_domain}\3|" "${SERVICES_FILE}" >"${SERVICES_FILE}.tmp" && mv "${SERVICES_FILE}.tmp" "${SERVICES_FILE}"
 
-echo "Domain updated: $new_domain"
+echo "Domain updated: ${new_domain}"
